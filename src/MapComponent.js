40e1ad53c38fb63c3230e06839a8d75c6f764e73
Aditya Soni <54634472+adityasoni2019@users.noreply.github.com>
2023-04-13 11:36:20 +0530

    some trivial changes

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -2,12 +2,10 @@ import React from "react";
 import GoogleMapReact from 'google-map-react';
 import html2canvas from "html2canvas";
 import { useState } from "react";
-import { useRef } from "react";
 import SceneWithSpinningBoxes from "./Cuboid";
 
 function MapComponent() {
 
-  const componentRef = useRef();
   const [dataUrl, setDataUrl] = useState("https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aW1hZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60");
   const [counter, setCounter] = useState(0);
 
@@ -46,7 +44,6 @@ function MapComponent() {
             attribute="allowTaint"
             type="boolean"
             default="false"
-            ref={componentRef}
           />
         </div>
         <SceneWithSpinningBoxes dataUrl={dataUrl} />
